b92f0b194e5d0669a904baa76cd64bc807e6441f
ahmadziadev <ziamain@proton.me>
2026-04-25 12:59:16 +0500

    Solve 06 repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, n) {
+    let ans = "";
+    if (n < 0) return "ERROR";
+    for (let i = 0; i < n; i++) {
+        ans += text;
+    }
+    return ans; 
 };
 
 // Do not edit below this line
