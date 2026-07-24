2fff7eeca9b85f2c4e8fe6ec42b8a1bd00ecdb1e
ahmadziadev <ziamain@proton.me>
2026-07-24 12:24:19 +0500

    Solve Exercise 15 - Find the Oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        if (a.yearOfDeath == undefined) a.yearOfDeath = 2026;
+        if (b.yearOfDeath == undefined) b.yearOfDeath = 2026;
+        return b.yearOfDeath - b.yearOfBirth - a.yearOfDeath + a.yearOfBirth;
+    }).at(0);
 };
 
 // Do not edit below this line
