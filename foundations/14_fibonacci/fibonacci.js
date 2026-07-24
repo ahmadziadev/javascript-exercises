2cf653d6d69a5cffe4153fd698fd24d1b872399a
ahmadziadev <ziamain@proton.me>
2026-07-24 12:09:08 +0500

    Solve Exercise 14 - Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0; 
+    for (let i = 3; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
