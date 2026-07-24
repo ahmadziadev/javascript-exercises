969d3f9200a0848a273ff09ff7a1819f0d887614
ahmadziadev <ziamain@proton.me>
2026-07-24 11:58:21 +0500

    Solve Exercise 13 - Palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleaned = Array.from(text.toLowerCase())
+    .filter(character => /[a-zA-Z0-9]/.test(character))
+    return cleaned.join("") == cleaned.reverse().join("");
 };
 
 // Do not edit below this line
